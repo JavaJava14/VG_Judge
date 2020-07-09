51d191e092ff032d378a347feae6e1eb9ddc5c84
Joey <54172386+JavaJava14@users.noreply.github.com>
2020-07-08 19:11:58 -0500

    Include validations for models

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,4 +1,5 @@
 class Game < ActiveRecord::Base
 has_many :favorites
 has_many :users, through: :favorites
+validates :name, :users, :favorites, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ActiveRecord::Base
 has_many :games
 has_many :favorites, through: :games
+validates :username, :password, presence: true
+validates_uniqueness_of :username
+has_secure_password
 end
