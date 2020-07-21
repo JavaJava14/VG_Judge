160818b8ba47ac517af5a5783489dd5fa794bf58
Joey <54172386+JavaJava14@users.noreply.github.com>
2020-07-21 14:57:33 -0500

    Update Game model with appropriate relationships. Accepts nested attributes, includes validations and scope

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,5 +1,8 @@
 class Game < ActiveRecord::Base
-has_many :favorites
-has_many :users, through: :favorites
-validates :title, :users, :favorites, presence: true
+  belongs_to :user
+  belongs_to :review
+  has_many :genres
+  accepts_nested_attributes_for :review
+  validates :title, :developer, :year, :genre_ids, presence: true
+  scope :search_by_title, -> (search_title){where("title = ?", search_title)}
 end
