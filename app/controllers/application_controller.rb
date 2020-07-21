ebcd0768aee7b41fbe5e7c208b9ef94c19bdda6e
Joey <54172386+JavaJava14@users.noreply.github.com>
2020-07-21 15:04:53 -0500

    Check user login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,20 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+  helper_method :current_user
+  helper_method :logged_in?
+
+  def current_user
+    @user = User.find_by(id: session[:user_id])
+  end
+
+  def logged_in?
+   !!current_user
+  end
+
+  def require_login
+    if !logged_in?
+      flash[:alert] = "Enter credentials to login"
+      redirect_to root_path
+    end
+  end
 end
