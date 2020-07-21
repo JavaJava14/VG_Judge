d086d44dac2e668e5c92a5235b52fc6f9b573f26
Joey <54172386+JavaJava14@users.noreply.github.com>
2020-07-21 15:03:33 -0500

    Genres for dropdown list

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+action = Genre.create(name: "Action")
+adventure = Genre.create(name: "Adventure")
+puzzle = Genre.create(name: "Puzzle")
+roleplay = Genre.create(name: "Role-play")
+simulation = Genre.create(name: "Simulation")
+sports = Genre.create(name: "Sports")
