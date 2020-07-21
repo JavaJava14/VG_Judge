b11636eca0baf37eb94c56cfdc9c5f264962e15a
Joey <54172386+JavaJava14@users.noreply.github.com>
2020-07-21 14:54:02 -0500

    Update User model to use has_many reviews through games

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
-has_many :games
-has_many :favorites, through: :games
-validates :username, :password, presence: true
-validates_uniqueness_of :username
-has_secure_password
+  has_many :games
+  has_many :reviews, through: :games
+  validates :username, :password, presence: true
+  validates_uniqueness_of :username
+  has_secure_password
 end
