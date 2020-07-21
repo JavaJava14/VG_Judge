6409b336c2646fe94d39702638cacd960ac35767
Joey <54172386+JavaJava14@users.noreply.github.com>
2020-07-21 14:52:21 -0500

    Setup routes that includes nested routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,20 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root "application#index"
+
+  resources :users
+  resources :games
+  resources :reviews
+  resources :sessions, only: [:new]
+
+  resources :users do
+    resources :games
+  end
+ 
+  get '/sessions', to: 'sessions#new'
+  post '/sessions', to: 'sessions#create'
+  get '/logout/:id', to: 'sessions#destroy'
+  delete '/logout/:id', to: 'sessions#destroy', as: 'logout'
+  get '/auth/:provider/callback', to: 'sessions#omniauth'
+  get '/most_user', to: "users#most_users", as: 'most_users'
 end
