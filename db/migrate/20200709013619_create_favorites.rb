9e5e3db322c66dccd6dfdcb805f461a0fb9d70d5
Joey <54172386+JavaJava14@users.noreply.github.com>
2020-07-21 14:47:00 -0500

    Delete 20200709013619_create_favorites.rb

--- db/migrate/20200709013619_create_favorites.rb
+++
@@ -1,9 +0,0 @@
-class CreateFavorites < ActiveRecord::Migration[6.0]
-  def change
-    create_table :favorites do |t|
-      t.integer :rating
-      t.integer :user_id
-      t.integer :game_id
-    end
-  end
-end
