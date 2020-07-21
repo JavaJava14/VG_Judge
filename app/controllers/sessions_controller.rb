8b4bc1fb9cbb266a94aa62e4842c4874212acb99
Joey <54172386+JavaJava14@users.noreply.github.com>
2020-07-21 15:00:38 -0500

    Setup CRUD for sessions controller. Also include secondary login source from github.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,40 @@
 class SessionsController < ApplicationController
 
+  def new
+    if !current_user
+      @user = User.new
+    else
+      redirect_to user_path(current_user)
+    end
+  end
+
+  def create
+    @user = User.find_by(username: params[:username])
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      redirect_to user_path(@user)
+    else
+      flash[:alert] = "Your login credentials were incorrect. Please try again."
+      redirect_to sessions_path(@user)
+    end
+  end
+
+  def destroy 
+    session.delete :user_id
+    redirect_to root_path
+  end
+
+  def omniauth
+    @user = User.from_omniauth(auth)
+    @user.save
+    session[:user_id] = @user.id
+    redirect_to user_path(@user)
+  end
+
+  private
+
+  def auth
+    request.env['omniauth.auth']
+  end
 
 end
